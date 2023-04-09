3e7f492c0c0963c0f29e09e3e9c3194375c6206a
Justin Ross <jross@apache.org>
2023-04-09 09:58:48 -0400

    WIP

--- config/config.py
+++ config/config.py
@@ -21,21 +21,27 @@ def table_button(content, href=None):
     else:
         return f"<a class=\"table-button\" href=\"{href}\">{content}</a>"
 
-def cell(column_index, value):
-    if column_index == 1:
-        value = html_link(value)
+## Site
 
-    return f"<td>{value}</td>"
-
-data = (
-    ("frontend", "http://frontend:8080"),
-    ("inventory", "http://inventory:8080"),
-    ("orders", "http://orders:8080"),
-    ("database", "jdbc://database:5432"),
-    ("reviews", "http://reviews:8080"),
+props = (
+    ("Name", "storefront"),
+    ("Ingress", "None"),
+    ("Console enabled?", "Yes"),
+    ("Routers", 1),
+    ("Router CPU request", 1.0),
+    ("Router CPU limit", 1.0),
 )
 
-connection_url_table = html_table(data, headings=("Service", "Connection URL"), cell_fn=cell, style="width: 50%;")
+site_settings = html_table(props, class_="properties")
+
+props = (
+    ("Version", "1.2.3"),
+    ("Created", "2 days ago"),
+)
+
+site_properties = html_table(props, class_="properties")
+
+## Links
 
 def cell(column_index, value):
     if column_index == 0:
@@ -63,59 +69,7 @@ data = (
 
 incoming_link_table = html_table(data, headings=headings)
 
-props = (
-    ("Name", "storefront"),
-    ("Ingress", "None"),
-    ("Console enabled?", "Yes"),
-    ("Routers", 1),
-    ("Router CPU request", 1.0),
-    ("Router CPU limit", 1.0),
-)
-
-site_settings = html_table(props, class_="properties")
-
-props = (
-    ("Version", "1.2.3"),
-    ("Created", "2 days ago",),
-)
-
-site_properties = html_table(props, class_="properties")
-
-def cell(column_index, value):
-    if column_index == 3:
-        value = table_button("Create a binding")
-
-    return f"<td>{value}</td>"
-
-headings = "Name", "Replicas", "Bindings", "Actions"
-
-data = (
-    ("frontend", 3, html_link("frontend-2da6", href=f"{site_url}/services/frontend/bindings/frontend-2da6/index.html"), None),
-    ("reviews", 2, "-", None),
-    ("orders", 2, "-", None),
-)
-
-deployment_table = html_table(data, headings=headings, cell_fn=cell)
-
-def cell(column_index, value):
-    if column_index == 0:
-        value = html_link(value, href="frontend/index.html")
-    elif column_index == 4:
-        value = table_button("Delete")
-
-    return f"<td>{value}</td>"
-
-headings = "Name", "Status", "Bindings", "Created", "Actions"
-
-data = (
-    ("frontend", "OK", 2, "3 minutes ago", None),
-    ("inventory", "OK", 1, "4 hours ago", None),
-    ("orders", "OK", 1, "2 days ago", None),
-    ("database", "OK", 1, "3/15/2020", None),
-    ("reviews", "Error: No bindings", 0, "3/15/2020", None),
-)
-
-service_table = html_table(data, headings=headings, cell_fn=cell)
+## Tokens
 
 def cell(column_index, value):
     if column_index == 0:
@@ -134,11 +88,55 @@ data = (
 
 token_table = html_table(data, headings=headings, cell_fn=cell)
 
-props = (
-    ("Name", "storefront-token-1"),
-    ("Use limit", 1),
-    ("Expiry", "15 minutes"),
-    ("Created", "3 minutes ago"),
+## Provided services
+
+def cell(column_index, value):
+    if column_index == 0:
+        value = html_link(value, href="provided-service/index.html")
+
+    return f"<td>{value}</td>"
+
+headings = "Name", "Ports", "Created"
+
+data = (
+    ("orders", "8080", "2 days ago"),
 )
 
-token_properties = html_table(props, class_="properties")
+provided_service_table = html_table(data, headings=headings, cell_fn=cell)
+
+props = (
+    ("Name", "orders"),
+)
+
+provided_service_settings = html_table(props, class_="properties")
+
+props = (
+    ("Created", "2 days ago"),
+)
+
+provided_service_properties = html_table(props, class_="properties")
+
+headings = "Port number", "Name", "Protocol", "Created"
+
+data = (
+    ("8080", "api",  "TCP", "2 days ago"),
+)
+
+provided_service_ports_table = html_table(data, headings=headings)
+
+## Required services
+
+def cell(column_index, value):
+    if column_index == 0:
+        value = html_link(value, href="required-service/index.html")
+
+    return f"<td>{value}</td>"
+
+headings = "Name", "Ports", "Created"
+
+data = (
+    ("inventory", "8080", "2 days ago"),
+    ("database", "5432, 8080", "2 days ago"),
+)
+
+required_service_table = html_table(data, headings=headings, cell_fn=cell)
